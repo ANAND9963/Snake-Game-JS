fa1974b8f7a20cd6017d0d7ffc14ff7a1af7188a
Anand <yannamanenianandkumar@gmail.com>
2024-10-16 20:46:07 -0400

    completed basic css styles in static javascript project

---
+++ script.js
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="style.css" />
+
+//const
+let highscore =0;
+let currscore =0;
+let snake =[{x:10,y:10},
+    {x:10, y:11},{x:10 , y:12},
+
+];
+
+let food =[{x:14, y:14}];
+let hasGAmeStarted = false;
+
+
+
+//methods
+
+const drawSnake = () => {
+
+}
+const generateFood = () => {
+
+}
+
+const move =() => {
+
+}
