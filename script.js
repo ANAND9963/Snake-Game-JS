32d6c1e755192929cf63654b1ee0941264636a08
Anand <yannamanenianandkumar@gmail.com>
2024-10-18 16:19:53 -0400

    basic changes

--- script.js
+++ script.js
@@ -1,142 +1,145 @@
-let highscore = 0;
-let currscore = 0;
-let snake = [
-  { x: 10, y: 10 },
-  { x: 10, y: 11 },
-  
-];
-
+let highScore = 0,
+  currscore = 0;
+let snake = [{ x: 10, y: 10 }];
 let dir = "right";
-let hasGAmeStarted = false;
+let hasGameStarted = false;
 let gridsize = 20;
-let gameInterval;
+let gameInteral;
 let gameSpeedDelay = 250;
 
-const logoel = document.getElementById("logo");
-const instructel = document.getElementById("instruction-text");
-const boardEl = document.getElementById("box");
 
-//methods
+const logoel = document.getElementById("logo");
+const instrutionel = document.getElementById("instruction-text");
+const boardEle = document.getElementById("box");
+const currscoreEl = document.getElementById("currscore");
+const highScoreEL = document.getElementById("highScore");
+
+currscoreEl.innerHTML = currscore.toString().padStart(3,"0");
+highScoreEL.innerHTML =highScore.toString().padStart(3,"0");
+
+//methods           
 const setPosition = (pixel, el) => {
   el.style.gridRow = pixel.x;
   el.style.gridColumn = pixel.y;
 };
-
-const createGameEl = (tag, className) => {
+const createGameElement = (tag, className) => {
   let el = document.createElement(tag);
   el.className = className;
-
   return el;
 };
-
 const generateFood = () => {
-  x = Math.floor(Math.random() * gridsize + 1);
+  x = Math.floor(Math.random() * gridsize + 1); // 0,1
   y = Math.floor(Math.random() * gridsize + 1);
+
   return { x, y };
 };
-
 let food = generateFood();
-
 const drawfood = () => {
-//   boardEl.innerHTML = "";
-  let foodSquare = createGameEl("div", "food");
+  let foodSquare = createGameElement("div", "food");
   setPosition(food, foodSquare);
-  boardEl.appendChild(foodSquare);
+  boardEle.appendChild(foodSquare);
 };
-
 const drawSnake = () => {
-  snake.map((pixel, idx) => {
-    let snakeSquare = createGameEl("div", "snake");
+  snake.map((pixel, index) => {
+    
+    let snakeSquare = createGameElement("div", "snake");
+   
     setPosition(pixel, snakeSquare);
-    boardEl.appendChild(snakeSquare);
+    boardEle.appendChild(snakeSquare);
   });
 };
-
-const draw = () => {
-  drawSnake();
-  drawfood();
-};
-
 const move = () => {
   let snakeHead = { x: snake[0].x, y: snake[0].y };
-  //eatfood
-  //onestep move
-  //dir where to go snake
+
   switch (dir) {
     case "up":
       snakeHead.x--;
-
       break;
     case "down":
       snakeHead.x++;
-
       break;
     case "left":
       snakeHead.y--;
-
       break;
     case "right":
       snakeHead.y++;
-
       break;
-
     default:
       break;
   }
   snake.unshift(snakeHead);
-
   if (food.x === snakeHead.x && food.y === snakeHead.y) {
-    //add head to the snake
     food = generateFood();
-  } else {
-    //sdd the snakehead
+    currscore = snake.length - 1; // Update score based on snake length
+    currscoreEl.innerHTML = currscore.toString().padStart(3,"0");
 
-    //remove tail
+  } else {
     snake.pop();
   }
 };
 const resetGame = () => {
-  hasGAmeStarted = false;
-  logoel.style.display = "block";
-  instructel.style.display = "block";
-  snake = [{ x: 10, y: 10 }];
-   
-  highscore = Math.max(highscore, currscore);
-  currscore = 0;
+  clearInterval(gameInteral);
+  hasGameStarted = false;
+  currscore =0;
+  currscoreEl.innerHTML = currscore.toString().padStart(3,"0");
+  logoel.style.display = "block"; 
+  instrutionel.style.display = "block";
   dir = "right";
-  clearInterval(gameInterval);
-};
+  snake = [{ x: 10, y: 10 }];
 
+
+  
+  
+};
+const updateScore = () => {
+
+    highScore = Math.max(highScore,currscore);
+    highScoreEL.innerHTML = highScore.toString().padStart(3,"0");
+    
+
+    
+   
+};
 const checkCollision = () => {
   if (
-    snake[0].x == gridsize + 1 ||
-    snake[0].y == gridsize + 1 ||
-    snake[0].x == 0 ||
-    snake[0].y == 0
+    snake[0].x === 0 ||
+    snake[0].x === gridsize + 1 ||
+    snake[0].y === 0 ||
+    snake[0].y === gridsize + 1
   ) {
-    //reset the ganme
+    updateScore();  
     resetGame();
-  }
-};
+    }
 
+    for(let i = 1; i < snake.length ; i++){
+        let head = snake[0];
+        if(head.x ==snake[i].x && head.y == snake[i].y){
+            updateScore();
+            resetGame();
+        }
+    }
+};
+const draw = () => {
+  drawSnake();
+  drawfood();
+};
 const startGame = () => {
   logoel.style.display = "none";
-  instructel.style.display = "none";
-  gameInterval = setInterval(() => {
-    boardEl.innerHTML = "";
+  instrutionel.style.display = "none";
+  snake = [{ x: 10, y: 10 }];
+  gameInteral = setInterval(() => {
     move();
-
+    boardEle.innerHTML = "";
     checkCollision();
     draw();
-
-    //score
-    //check collision
   }, gameSpeedDelay);
 };
 
-const handleKeyPres = (e) => {
+//events
+const handleKeyPress = (e) => {
+  // e.preventDefault();
   if (e.key === " " || e.key === "Space") {
-    hasGAmeStarted = true;
+    hasGameStarted = true;
     startGame();
   } else {
     switch (e.key) {
@@ -158,4 +161,4 @@ const handleKeyPres = (e) => {
   }
 };
 
-document.addEventListener("keyup", handleKeyPres);
+document.addEventListener("keyup", handleKeyPress);
